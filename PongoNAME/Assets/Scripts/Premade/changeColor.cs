16779dfc427a25f88483a6ab21b2599c2ebda0e9
Rushil Umaretiya <rushilwiz@gmail.com>
2018-10-03 23:36:06 -0400

    Initial Upload

---
+++ PongoNAME/Assets/Scripts/Premade/changeColor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeColor : MonoBehaviour {
+
+	[SerializeField]
+	SpriteRenderer rend;
+
+	[SerializeField]
+	Text Title;
+
+	void Start () {
+
+		//Gets Renderer
+		rend = GetComponent<SpriteRenderer>();
+	}
+
+	void FixedUpdate () {
+		//Calls rainbowEffect
+		rainbowEffect ();
+
+	}
+
+	void rainbowEffect() {
+
+			Color rainbowColor = new Color (
+				Random.value,
+				Random.value,
+				Random.value
+			);
+
+			if (rend != null) {
+				rend.color = rainbowColor;
+			}
+
+			if (Title != null) {
+				Title.color = rainbowColor;
+			}
+	}
+}
